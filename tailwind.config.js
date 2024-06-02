94795577fbee6fb135d304d3f1cee8bbcc61c3b2
Hossam Bhi Pasha <hossambasha9900@gmail.com>
2024-06-02 17:05:55 +0300

    Init project and install packages

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3700B3',
+      },
+    },
+  },
+  plugins: [],
+};
